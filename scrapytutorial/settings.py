679d6ad1f4c508151ea569f12517ea69ab71e240
James Maringa <jmsmaringa@gmail.com>
2014-12-28 13:37:42 +0300

    initial project creation

---
+++ scrapytutorial/settings.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+# Scrapy settings for scrapytutorial project
+#
+# For simplicity, this file contains only the most important settings by
+# default. All the other settings are documented here:
+#
+#     http://doc.scrapy.org/en/latest/topics/settings.html
+#
+
+BOT_NAME = 'scrapytutorial'
+
+SPIDER_MODULES = ['scrapytutorial.spiders']
+NEWSPIDER_MODULE = 'scrapytutorial.spiders'
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+#USER_AGENT = 'scrapytutorial (+http://www.yourdomain.com)'
